972013237084b03861006407a760f7b99d32f827
Arjun Surendra <asurendra@vmware.com>
2021-10-27 14:08:24 +0530

    code

--- ui/src/app/components/home/home.component.ts
+++ ui/src/app/components/home/home.component.ts
@@ -1,51 +1,63 @@
-import { Component, OnInit } from '@angular/core';
-import { Customer } from '../../models/customer';
-import { RestService } from '../../services/rest.service';
-import { Router } from '@angular/router';
-import { ClarityIcons, trashIcon, userIcon } from '@cds/core/icon';
+import {Component, OnInit} from '@angular/core';
+import {Customer} from '../../models/customer';
+import {RestService} from '../../services/rest.service';
+import {Router} from '@angular/router';
+import {ClarityIcons, trashIcon} from '@cds/core/icon';
+import {ClrDatagridStateInterface} from '@clr/angular';
+import {CustomerPage} from "../../models/customer-page";
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: []
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: []
 })
 export class HomeComponent implements OnInit {
 
-    customers: Customer[] = [];
-    customer: Customer = new Customer();
-    flashMsg = '';
+  customerPage: CustomerPage = new CustomerPage();
+  customer: Customer = new Customer();
+  flashMsg = '';
+  loading = false;
+  page: number = 1;
 
-    currentTime = new Date();
-    constructor(private restService: RestService, private router: Router) {
-        ClarityIcons.addIcons(trashIcon);
-    }
+  constructor(private restService: RestService, private router: Router) {
+    ClarityIcons.addIcons(trashIcon);
+  }
 
-    ngOnInit(): void {
-        this.getCustomers();
-        this.flashMsg = '';
-    }
+  ngOnInit(): void {
+    this.loading = true;
+    this.flashMsg = '';
+  }
 
-    getCustomers(): void {
-        this.customer = new Customer();
-        this.restService.getCustomers().subscribe(data => {
-            this.customers = data;
-        });
-    }
+  saveCustomer(): void {
+    this.restService.saveCustomer(this.customer)
+      .subscribe(data => {
+        this.flashMsg = 'Saved customer: ' + this.customer.firstName;
+      });
+  }
 
-    saveCustomer(): void {
-        this.restService.saveCustomer(this.customer)
-            .subscribe(data => {
-                this.flashMsg = 'Saved customer: ' + this.customer.firstName;
-                this.getCustomers();
-            });
-    }
+  deleteCustomer(customer: Customer): void {
+    console.log('delete: ' + customer.id);
+    this.restService.deleteCustomer(customer.id)
+      .subscribe(data => {
+        this.flashMsg = 'Deleted customer: ' + customer.id;
+      });
+  }
 
-    deleteCustomer(customer: Customer): void {
-        console.log('delete: ' + customer.id);
-        this.restService.deleteCustomer(customer.id)
-            .subscribe(data => {
-                this.flashMsg = 'Deleted customer: ' + customer.id;
-                this.getCustomers();
-            });
+  refresh(state: ClrDatagridStateInterface) {
+    this.loading = true;
+    if (!state.page) {
+      state.page = {
+        from: 0,
+        to: 10,
+        size: 10
+      };
     }
+    this.restService.getCustomers(state.page.current, state.page.size).subscribe(data => {
+        this.customerPage = data;
+        this.loading = false;
+      },
+      error => {
+        this.loading = false;
+      });
+  }
 }
--- ui/src/app/services/rest.service.ts
+++ ui/src/app/services/rest.service.ts
@@ -1,33 +1,27 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Customer } from '../models/customer';
-import { Router } from '@angular/router';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Customer} from '../models/customer';
+import {Router} from '@angular/router';
+import {CustomerPage} from "../models/customer-page";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class RestService {
 
-    constructor(private http: HttpClient, private router: Router) {
-    }
+  constructor(private http: HttpClient, private router: Router) {
+  }
 
-    public getCustomers(): Observable<Customer[]> {
-        const token = 'Bearer ' + localStorage.getItem('token');
-        const headers = new HttpHeaders().set('Authorization', token);
-        return this.http.get<Customer[]>('/api/customer', { headers });
-    }
+  public getCustomers(page: number, size: number): Observable<CustomerPage> {
+    return this.http.get<CustomerPage>(`/api/customer?page=${page}&size=${size}`);
+  }
 
-    public saveCustomer(customer: Customer): Observable<any> {
-        const token = 'Bearer ' + localStorage.getItem('token');
-        const headers = new HttpHeaders().set('Authorization', token);
-        return this.http.post('/api/customer', customer, { headers });
-    }
+  public saveCustomer(customer: Customer): Observable<any> {
+    return this.http.post('/api/customer', customer);
+  }
 
-    public deleteCustomer(id: any): Observable<any> {
-        const token = 'Bearer ' + localStorage.getItem('token');
-        const headers = new HttpHeaders().set('Authorization', token);
-        return this.http.delete('/api/customer/' + id, { headers });
-    }
-
+  public deleteCustomer(id: any): Observable<any> {
+    return this.http.delete('/api/customer/' + id);
+  }
 }
